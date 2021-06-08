b895094b45e7076ed66ca3b60351661159be6bd7
nhlanhlaDEV <dhlamininhlanhla1@gmail.com>
2021-06-08 17:48:12 +0200

    first commit

--- task3.php
+++ task3.php
@@ -12,13 +12,12 @@
   <label for="height">Height:</label>
   <input type="text" id="height" name="height" placeholder="Please Enter Height" ><br>
 
-  <p>Please  choose your weight in kilograsm or pounds:</p>
-  <input type="radio" id="weight" name="radioWeight" value="weight">Pounds
- <input type="radio" id="weight" name="radioWeight" value="weight">Kilograms
-<br>
-<p>Please  choose your Height in meters or inches:</p>
-  <input type="radio" id="Height" name="radioHeight" value="height">Inches
- <input type="radio" id="Height" name="radioHeight" value="height">Kilograms
+  <p>Please  choose your BMI :</p>
+  <input type="radio" id="normal_bmi" name="radioBMI" value="1">
+  <label for="normal_bmi">BMI-In-Kg and Meters</label>
+  <br>
+ <input type="radio" id="bmi_inches" name="radioBMI" value="2">
+ <label for="bmi_inches">BMI-In-Pounds and Inches</label>
 <br>
 <br>
   <input type="submit" name="submit" value="Submit">
@@ -32,41 +31,68 @@
    $enterWeight = $_POST['weight'];
    $enterHeight = $_POST['height'];
 
+  //  $radioWeight = $_POST['radioWeight'];
+  //  $radioHeight = $_POST['radioHeight'];
 
-   // variables for radio buttons
-   $clickedWeight = $_POST['radioWeight'];
-   $clickedHeight = $_POST['radioHeight'];
 
-   // 
-   $mass = $_POST['weight'];
-   $heightValue = $_POST['height'];
+  $totalBMI = ($enterWeight /($enterHeight * $enterHeight));
+
+  $bmi = $enterWeight *  0.453/($enterHeight * $enterHeight) * 0.025;
+
+
+   $message = "";
 
    //variable checking weather the data inserted is empty or less than sero or  equal to zero and string message
    $weightMessage = $enterWeight == 0 ||  $enterWeight < 0 || empty($enterWeight) || $enterWeight == "";
    $heightMessage = $enterHeight == 0 ||  $enterHeight  < 0 || empty($enterHeight) || $enterHeight  == "";
 
-// checks that button submit is clicked
-   if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-// echo data 
-    if($weightMessage && $heightMessage){
+// checks that button submit is clicked 
+ if(isset($_POST['submit'])){
+
+  if($weightMessage && $heightMessage){
           
-         echo "PLEASE HEIGHT AND WEIGHT  FIELDS SHOULD NOT BE ZERO DIGITS OR STRING OR EMPTY";
-     }
+    echo "PLEASE HEIGHT AND WEIGHT  FIELDS SHOULD NOT BE ZERO DIGITS OR STRING OR EMPTY";
+}
 
+if($_POST['radioBMI'] == 1){
 
- }
+   if($totalBMI < 18.5){
+    $message = " you are underweight";
+    echo "your body mass is index(BMI): ". round($totalBMI,2) . "<br> " . $message;
 
+   }elseif($totalBMI > 18.5 && $totalBMI <= 24.9){
+    $message = " You are healthy weight";
+    echo "your body mass is index(BMI): ". round($totalBMI,2) . "<br> " . $message;
 
- // function calculate weight and height in killograms and meters
- function calcBMI ($mass,$heightValue){
-        
-     $totalBMI = ($mass /($heightValue * $heightValue));
- }
+   }elseif($totalBMI >= 25 && $totalBMI <= 29.9){
+    $message = "You are Overweight";
+    echo "your body mass is index(BMI): ". round($totalBMI,2) . "<br> " . $message;
+   }elseif($totalBMI >= 30){
+    $message = "You are obese";
+    echo "your body mass is index(BMI): ". round($totalBMI,2) . "<br> " . $message;
+   }
 
-
-
-
+  }
+  if($_POST['radioBMI'] == 2){
+   
+    if($bmi < 18.5){
+      $message = "you are underweight";
+      echo " body mass  index(BMI): ". round($bmi,2) . "<br> " . $message;
+  
+     }elseif($bmi > 18.5 && $bmi <= 24.9){
+      $message = " You are healthy weight";
+      echo " body mass index(BMI): ". round($bmi,2) . "<br> " . $message;
+  
+     }elseif($bmi >= 25 && $bmi <= 29.9){
+      $message = "You are Overweight";
+      echo " body mass  index(BMI): ". round($bmi,2) . "<br> " . $message;
+     }elseif($bmi >= 30){
+      $message = "You are obese";
+      echo " body mass index(BMI): ". round($bmi,2) . "<br> " . $message;
+      }
+   }
+}
 
  ?>
  
